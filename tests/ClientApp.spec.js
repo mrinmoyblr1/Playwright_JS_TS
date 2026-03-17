b67973d7abb603bbe3cab39a8be33b004468c536
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-03-17 17:34:59 -0400

    BISWAS

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,9 +1,7 @@
 const { test, expect } = require('@playwright/test')
-
 test.only('Browser Context Playwright Test', async ({ page }) => {
     const productName = 'ZARA COAT 3';
     const products = page.locator('.card-body');
-
     await page.goto('https://rahulshettyacademy.com/client');
     await page.locator('#userEmail').fill('mrinmoy.blr@gmail.com');
     await page.locator('#userPassword').fill('Test1234');
@@ -15,10 +13,8 @@ test.only('Browser Context Playwright Test', async ({ page }) => {
     await page.locator('.card-body b').first().waitFor();
     await page.locator('.card-body b').last().waitFor();
     await page.locator('.card-body b').nth(1).waitFor();
-
     // const titles = page.locator('.card-body b').allTextContents();
     // console.log(await titles);
-
     const count = await products.count();
     for (let i = 0; i < count; ++i) {
         if (await products.nth(i).locator('b').textContent() === productName) {
@@ -28,6 +24,21 @@ test.only('Browser Context Playwright Test', async ({ page }) => {
     }
 
     await page.locator("[routerlink*='cart']").click();
-    await page.pause(3000);
-
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator("text=Checkout").click();
+    await page.locator("[placeholder*='Country']").pressSequentially("Ind");
+    //const options = page.locator("section").nth(1);
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+    const optionCount = await dropdown.locator("button").count();
+    for (let i = 0; i < optionCount; i++) {
+        const text = await dropdown.locator("button").nth(i).textContent();
+        if (text.trim() === "India") {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+    //await page.pause();
 });
