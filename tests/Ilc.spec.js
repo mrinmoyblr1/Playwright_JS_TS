614ff7661b53af6ae9c0a28b1e63aae17a7e632e
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-18 17:35:23 -0400

    BISWAS

--- tests/Ilc.spec.js
+++ tests/Ilc.spec.js
@@ -1,12 +1,25 @@
-const { test, expect } = require('@playwright/test');
+//const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
 test.only('Playwright Special Locator', async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel("Check me out if you Love IceCreams!").click();
+
+    // In below, we can perform .click() or .check()
     await page.getByLabel("Employed").click();
     await page.getByLabel("Employed").check();
+
+    // This is getByLabel() then Select Option
     await page.getByLabel("Gender").selectOption("Female");
+
+    // getByPlaceholder
     await page.getByPlaceholder("Password").fill("ASD");
+
+    await page.getByPlaceholder("Password").fill("ASD1");
+
+    // getByRole : we need one extra parameter for {name:'Submit'}
     await page.getByRole("button", { name: 'Submit' }).click();
+
     await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
     await page.getByRole("Link", { name: 'Shop' }).click();
     await page.locator("app-card").filter({ hasText: 'Nokia Edge' }).getByRole("button").click();
